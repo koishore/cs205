cfe22505896effe39d887a24e6fb5c5fd1f54024
Koishore Roy <koishore.roy@ashoka.edu.in>
2016-03-17 23:29:35 +0530

    Third commit
    Added feature to display number of files recursively under each
    directory

--- Assignment1/directory.java
+++ Assignment1/directory.java
@@ -1,7 +1,11 @@
+//
+//
+//Created by Koishore Roy on 10/03/2016.
+
 import java.io.File;
 
 public class directory {
-    public void listd (String dirPath, int level) {
+    public void listd (String dirPath) {
         File dir = new File(dirPath);
         File[] first = dir.listFiles();
         
@@ -9,13 +13,16 @@ public class directory {
             for (File aFile : first) {
                 
                 if (aFile.isDirectory()) {
-                    int c = countFiles (aFile);
-                    System.out.println(c + " " + "d" + " " + aFile.getAbsolutePath());
-                    listd(aFile.getAbsolutePath(), level + 1);
+                    int c1 = countFiles (aFile);
+                    int c2 = countFilesrec (aFile);
+                    System.out.println(c2 + " " + c1 + " " + "d" + " " + aFile.getAbsolutePath());
+                    listd(aFile.getAbsolutePath());
                 }
                 
                 else {
-                    System.out.println("0" + " " + "f" + " " + aFile.getAbsolutePath());
+                    int c1 = 0;
+                    int c2 = 0;
+                    System.out.println(c2 + " " + c1 + " " + "f" + " " + aFile.getAbsolutePath());
                 }
             }
         }
@@ -24,19 +31,29 @@ public class directory {
     public static int countFiles (File direct) {
         int count = 0;
         for (File file : direct.listFiles()) {
-            if (file.isDirectory()) {
-                count = count;
-            }
-            else {
+            if (!file.isDirectory()) {
                 count++;
             }
         }
         return count;
     }
     
-    public static void main(String[] args) {
+    public static int countFilesrec (File direct) {
+        int count = 0;
+        for (File file : direct.listFiles()) {
+            if (file.isDirectory()) {
+                count += countFilesrec(file);
+            }
+            else
+                count++;
+        }
+        return count;
+    }
+
+    
+    public static void main (String[] args) {
         directory prog = new directory();
-        String list = System.getProperty("user.home") + File.separator + "Documents";
-        prog.listd(list, 0);
+        String list = System.getProperty("user.home") + File.separator + "Desktop" + File.separator + "travicin";
+        prog.listd(list);
     }
 }
