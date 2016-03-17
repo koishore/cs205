4256ab591d97f1b78a9af2af07cec8a2b8bfa5d9
Koishore Roy <koishore.roy@ashoka.edu.in>
2016-03-18 04:18:28 +0530

    fourth commit
    added feature to check number of each file types as well as file sizes.

--- Assignment1/directory.java
+++ Assignment1/directory.java
@@ -1,12 +1,19 @@
 //
 //
 //Created by Koishore Roy on 10/03/2016.
+//
+//
+//directory.java
 
 import java.io.File;
+import java.lang.String;
 
 public class directory {
+    
+    static String[] TYPES = new String[] {"jpg", "png", "gif", "mp4", "mp3", "exe", "psd", "html", "xml"};
+    
     public void listd (String dirPath) {
-        File dir = new File(dirPath);
+        File dir = new File (dirPath);
         File[] first = dir.listFiles();
         
         if (first != null && first.length > 0) {
@@ -16,16 +23,72 @@ public class directory {
                     int c1 = countFiles (aFile);
                     int c2 = countFilesrec (aFile);
                     System.out.println(c2 + " " + c1 + " " + "d" + " " + aFile.getAbsolutePath());
-                    listd(aFile.getAbsolutePath());
+                    listd (aFile.getAbsolutePath());
                 }
                 
                 else {
                     int c1 = 0;
                     int c2 = 0;
-                    System.out.println(c2 + " " + c1 + " " + "f" + " " + aFile.getAbsolutePath());
+                    System.out.println (c2 + " " + c1 + " " + "f" + " " + aFile.getAbsolutePath());
                 }
             }
         }
+    }
+
+    public void extension (File cwd) {
+        for (String ext: TYPES) {
+            int count = 0;
+            long size = 0;
+            
+            for (File file : cwd.listFiles()) {
+                if (file.isFile()) {
+                    if (file.toString().endsWith(ext)) {
+                        count++;
+                        size = size + file.length();
+                    }
+                }
+                
+                else {
+                    count += extrecurse (file, ext);
+                    size += extrecursesize (file, ext);
+                }
+            }
+            
+            System.out.println (ext + " - " + count + " " + String.format("%,d", size));
+        }
+    }
+
+    public int extrecurse (File director, String extension) {
+        int count = 0;
+        
+        for (File file: director.listFiles()) {
+            if (!file.isDirectory()) {
+                if (file.toString().endsWith(extension)) {
+                    count++;
+                }
+            }
+            
+            else {
+                count += extrecurse(file, extension);
+            }
+        }
+        return count;
+    }
+
+    public int extrecursesize (File director, String extension) {
+        long size = 0;
+        
+        for (File file : director.listFiles()) {
+            if (!file.isDirectory()) {
+                if (file.toString().endsWith(extension)) {
+                    size += file.length();
+                }
+            }
+            else {
+                size += extrecursesize (file, extension);
+            }
+        }
+        return (int) size;
     }
     
     public static int countFiles (File direct) {
@@ -49,11 +112,16 @@ public class directory {
         }
         return count;
     }
-
     
     public static void main (String[] args) {
+        long start = System.currentTimeMillis();
         directory prog = new directory();
-        String list = System.getProperty("user.home") + File.separator + "Desktop" + File.separator + "travicin";
+        String list = System.getProperty("user.home") + File.separator + "Desktop";
+        File list1 = new File(list);
         prog.listd(list);
+        prog.extension(list1);
+        long end = System.currentTimeMillis();
+        long time = end - start;
+        System.out.println ("Program executed in " + time + " milliseconds.");
     }
 }
